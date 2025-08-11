971035abdb4b7e40d9390d05dd74c8bf29d76f50
Johl Brown <johlbrown@gmail.com>
2025-08-11 19:25:38 +1000

    Add basic Bevy app

---
+++ bevy_app/src/main.rs
@@ -0,0 +1,15 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
